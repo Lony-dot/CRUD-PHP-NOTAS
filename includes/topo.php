7395b07a72c149519d13f0e60063c26fe1cb13be
Lony <loonydarck@hotmail.com>
2023-01-27 16:33:22 -0300

    Realizando reaproveitamento de código, criação do includes

---
+++ includes/topo.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link href="http://localhost/sisnotas/plugins/fontawesome-free-6.2.1-web/css/all.css" rel="stylesheet">
+
+    <title>Sisnotas</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Sisnotas</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Disciplinas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Notas</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
--- index.php
+++ index.php
@@ -1,51 +1,18 @@
-<!doctype html>
-<html lang="pt-BR">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+require __DIR__ . "\includes/topo.php";
+?>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <link href="http://localhost/sisnotas/plugins/fontawesome-free-6.2.1-web/css/all.css" rel="stylesheet">
-
-    <title>Sisnotas</title>
-  </head>
-  <body>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Sisnotas</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Disciplinas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Notas</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="container">
-
-      <div class="card mt-3">
-        <div class="card-header">
-          Home
-        </div>
-        <div class="card-body">
-          <h1 class="card-title">Sistema de Registro de Notas</h1>
-          <p class="card-text">Seja bem-vindo(a) ao sistema para registro de notas</p>
-        </div>
-      </div>
-
+<div class="card mt-3">
+  <div class="card-header">
+      Home
     </div>
+    <div class="card-body">
+      <h1 class="card-title">Sistema de Registro de Notas</h1>
+      <p class="card-text">Seja bem-vindo(a) ao sistema para registro de notas</p>
+  </div>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="http://localhost/sisnotas/plugins/jquery-3.6.3/jquery-3.6.3.min.js"></script>
-</body>
-</html>
+      <?php
+require __DIR__ . "\includes/fundo.php";
+?>
+
